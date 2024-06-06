f7b84b1223c894f0d14e507035ba177f6d98af62
viruskizz <kizzaraiva@gmail.com>
2024-06-07 01:08:59 +0700

    fix: eslint

--- src/app/components/codeinputfield.tsx
+++ src/app/components/codeinputfield.tsx
@@ -19,44 +19,24 @@ export const shakeAnimation = {
   },
 };
 
-// export default function CodeInputField({ value, onChange, error, autoFocus }: CodeInputProps) {
-//   return (
-//     <motion.input
-//       type="text"
-//       className={cn(
-//         "w-12 md:w-14 h-12 md:h-14 text-center text-xl sm:text-2xl font-extrabold text-gray-900 bg-white border border-gray-300 hover:border-gray-400 appearance-none rounded p-2 outline-none focus:bg-white focus:border-blue-500 focus:ring-2 focus:ring-blue-200",
-//         error ? "border-red-500 focus:border-red-500 focus:ring-red-200" : ""
-//       )}
-//       maxLength={1}
-//       required
-//       value={value}
-//       onChange={onChange}
-//       animate={error ? "shake" : ""}
-//       variants={shakeAnimation}
-//       autoFocus={autoFocus}
-//     />
-//   );
-// }
+const CodeInputField = forwardRef(({ value, onChange, error, autoFocus }: CodeInputProps, ref: any) => (
+  <motion.input
+    ref={ref}
+    type="text"
+    className={cn(
+      "w-12 md:w-14 h-12 md:h-14 text-center text-xl sm:text-2xl font-extrabold text-gray-900 bg-white border border-gray-300 hover:border-gray-400 appearance-none rounded p-2 outline-none focus:bg-white focus:border-blue-500 focus:ring-2 focus:ring-blue-200",
+      error ? "border-red-500 focus:border-red-500 focus:ring-red-200" : ""
+    )}
+    maxLength={1}
+    required
+    value={value}
+    onChange={onChange}
+    animate={error ? "shake" : ""}
+    variants={shakeAnimation}
+    autoFocus={autoFocus}
+  />
+));
 
-
-const CodeInputField = forwardRef(({ value, onChange, error, autoFocus }: CodeInputProps, ref: any) => {
-  return (
-    <motion.input
-      ref={ref}
-      type="text"
-      className={cn(
-        "w-12 md:w-14 h-12 md:h-14 text-center text-xl sm:text-2xl font-extrabold text-gray-900 bg-white border border-gray-300 hover:border-gray-400 appearance-none rounded p-2 outline-none focus:bg-white focus:border-blue-500 focus:ring-2 focus:ring-blue-200",
-        error ? "border-red-500 focus:border-red-500 focus:ring-red-200" : ""
-      )}
-      maxLength={1}
-      required
-      value={value}
-      onChange={onChange}
-      animate={error ? "shake" : ""}
-      variants={shakeAnimation}
-      autoFocus={autoFocus}
-    />
-  );
-})
+CodeInputField.displayName = 'CodeInputField';
 
 export default CodeInputField;
